f322f1295615325a087ad50f3dea801b4290fd16
newt0 <py_lambda@yahoo.co.jp>
2020-12-29 09:35:31 +0900

    add: Load  imageClassifier and image

--- sketch.js
+++ sketch.js
@@ -3,3 +3,9 @@ let classifier;
 
 // A variable to hold the image we want to classify
 let img;
+
+// Use p5's preload() function to load our imageClassifier model and our bird image before running the rest of our code
+function preload() {
+  classifier = ml5.imageClassifier("MobileNet");
+  img = loadImage("images/bird.png");
+}
